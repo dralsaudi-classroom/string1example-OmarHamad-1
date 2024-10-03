634f82481c3d20370313b6beb040f98e0bc4b074
OMER HAMAD ABELRAHMAN MOHAMMED <444100088@student.ksu.edu.sa>
2024-10-03 18:10:17 +0300

    omar hamad

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -1,54 +1,62 @@
 package com.example.project;
 
 public class String1 {
-	private char[] str;
-	private int size;
+    private char[] str;
+    private int size;
 
-	public String1 () {
-	   size = -1;
-	   str = new char[80];
-	}
-	public void append(char c) {
+    public String1() {
+        size = -1;
+        str = new char[80];
+    }
+
+    public void append(char c) {
         str[++size] = c;
     }
+
     public char remove() {
         return str[size--];
     }
+
     public char getChar(int i) {
         return str[i];
     }
+
     public int length() {
-        return size+1;
+        return size + 1;
     }
-    public void makeEmpty()
-    {
+
+    public void makeEmpty() {
         size = -1;
     }
-    public void concat(String1 s)
-    {
-        for(int i = 0; i < s.length(); i++)
-            append(s.getChar(i));
-    }
-    public boolean equal(String1 s) {
-    if (s.length() == this.length()) {
+
+    public void concat(String1 s) {
         for (int i = 0; i < s.length(); i++) {
-            if (s.getChar(i) != this.str[i]) {  
-                return false;
-            }
+            append(s.getChar(i));
         }
-        return true;
     }
-    return false;
-}
+
+    public boolean equal(String1 s) {
+        if (s.length() == this.length()) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.getChar(i) != this.str[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
     public void reverse() {
-	if(this.length()==0)
-		return;
-        int l = this.length()-1;
-	char tmp;
-	for(int i =0; i<l/2;i++){
-		tmp= this.str[i];
-		this.str[i]=this.str[l];
-		this.str[l--]=tmp;
-	    
+        if (this.length() == 0)
+            return;
+
+        int l = this.length() - 1;
+        char tmp;
+        for (int i = 0; i < l / 2; i++) {
+            tmp = this.str[i];
+            this.str[i] = this.str[l - i]; 
+            this.str[l - i] = tmp;  
+        }
     }
 }
